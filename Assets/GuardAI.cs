242c34ee3fb6c12e5cfb0f81506e6d1b4509426d
Sonat Karan <sonat.karan@infosfer.com>
2016-01-15 14:45:53 +0200

    Guard AI - initial

---
+++ Assets/GuardAI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardAI : MonoBehaviour {
+    public enum GUARD_AI{
+        IDLE,
+        ON_ALERT,
+        NOTICED,
+        SLEEP,
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
