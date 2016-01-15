21e6325f94b72f8464e734c81235cd1e70c2955f
Sonat Karan <sonat.karan@infosfer.com>
2016-01-15 22:11:29 +0200

    GuardAI - Update

--- Assets/GuardAI.cs
+++ Assets/GuardAI.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class GuardAI : MonoBehaviour {
-    public float speed = 5f;
+    public GameObject _monkey;
+    public GameObject _sloth;
+    public GameObject _monkeyCarrySloth;
+    public GameObject _slothCarryMonkey;
+   
+    [HideInInspector]
+    public bool _isSlothOutOfHisCage = false;
+    private bool _moveToTargetReached = false;
+    public float speed = 10f;
     private float _noticedTimeCounter = 0;
+    private Vector3 _initialPosition;
     private Vector3 _lastSoundPosition;
     private GUARD_AI _guardFlow = GUARD_AI.SLEEP;
     
     public enum GUARD_AI{
         SLEEP,
-        IDLE,
-        ON_ALERT,
+        BACK_TO_DESK,
         NOTICED,
+        ON_ALERT,
+        ACTION,
     }
 
 	// Use this for initialization
 	void Start () {
+        _initialPosition = transform.position;
 	   GameController.SoundHeard += onSoundHeard;
 	}
 	
@@ -29,40 +39,71 @@ public class GuardAI : MonoBehaviour {
             case GUARD_AI.SLEEP:
                 Sleep();
                 break;
-            case GUARD_AI.IDLE:
-                Idle();
-                break;
-            case GUARD_AI.ON_ALERT:
-                onAlert();
+            case GUARD_AI.BACK_TO_DESK:
+                BackToDesk();
                 break;
             case GUARD_AI.NOTICED:
                 Noticed();
                 break;
+            case GUARD_AI.ON_ALERT:
+                onAlert();
+                break;
+            case GUARD_AI.ACTION:
+                Noticed();
+                break;
+            
         }
     }
     private void Sleep(){
         Debug.Log("GuardAI::Sleep");
     }
-    private void Idle(){
-        Debug.Log("GuardAI::Idle");
+    private void BackToDesk(){
+        Debug.Log("GuardAI::BackToDesk");
+        if(transform.position != _initialPosition){
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(_initialPosition.x, transform.position.y, _initialPosition.z), step);    
+        }else{
+            goToSleep();
+        }
     }
     private void onAlert(){
         Debug.Log("GuardAI::onAlert");
-        MoveToSoundPosition();
+        if(_isSlothOutOfHisCage){
+            Debug.Log("GuardAI::_isSlothOutOfHisCage::true");
+            MoveTo(_sloth.transform.position);
+        }else{
+            Debug.Log("GuardAI::_isSlothOutOfHisCage::false");
+            if(_moveToTargetReached){
+                Debug.Log("GuardAI::_moveToTargetReached::true");
+                _moveToTargetReached = false;
+                _guardFlow = GUARD_AI.BACK_TO_DESK;
+            }else{
+                Debug.Log("GuardAI::_moveToTargetReached::false");
+                MoveTo(_lastSoundPosition);    
+            }
+            
+        }
     }
     private void Noticed(){
         Debug.Log("GuardAI::Noticed");
         _noticedTimeCounter += Time.deltaTime;
-        if(_noticedTimeCounter >= 5.0f){
+        if(_isSlothOutOfHisCage){
+            _guardFlow = GUARD_AI.ON_ALERT;
+            _noticedTimeCounter = 0;
+            _isSlothOutOfHisCage = false;
+        }
+        else if(_noticedTimeCounter >= 5.0f){
             goToSleep();
             _noticedTimeCounter = 0;
         }
     }
     
-    private void MoveToSoundPosition(){
-        if(Vector3.Distance(transform.position, _lastSoundPosition) > 0.1f){
+    private void MoveTo(Vector3 position){
+        if(Mathf.Abs(transform.position.x - position.x) > 0.5f){
             float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(_lastSoundPosition.x, transform.position.y, _lastSoundPosition.z), step);    
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(position.x, transform.position.y, position.z), step);    
+        }else{
+            _moveToTargetReached = true;
         }
     }
     
@@ -76,4 +117,5 @@ public class GuardAI : MonoBehaviour {
         _noticedTimeCounter = 0;
         _guardFlow = GUARD_AI.SLEEP;
     }
+    
 }
---
+++ Assets/LevelOneCheckpoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelOneCheckpoint : MonoBehaviour {
+    Flow _flow;
+    public GameObject _guard;
+	// Use this for initialization
+	void Start () {
+	   _flow = GameObject.Find("Game").GetComponent<Flow>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag == "Player"){
+            _guard.GetComponent<GuardAI>()._isSlothOutOfHisCage = true;
+            _flow._playerMonkey.GetComponent<Monkey>().Scream();
+            Destroy(gameObject);    
+        }
+    }
+}
