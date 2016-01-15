415dd2c81c002459cb671807fdeffb50ca51ad45
Sonat Karan <sonat.karan@infosfer.com>
2016-01-15 22:11:14 +0200

    Monkey Script added

---
+++ Assets/Monkey.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monkey : MonoBehaviour {
+    GameController _gameCon;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public void Scream(){
+        _gameCon = GameObject.Find("Game").GetComponent<GameController>();
+        //TODO(sonat): Play monkey sound here.
+        _gameCon.onSoundHeard(transform.position);
+    }
+}
